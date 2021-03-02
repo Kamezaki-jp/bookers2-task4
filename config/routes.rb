9da7241d43d4e3d2821037e1ea28c15b79fe38f9
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-02 10:54:16 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'home#top'
+  get 'home/about'
+
+  resources :users
+  resources :books do
+    resource    :favorites,     only: [:create, :destroy]
+    resources   :book_comments, only: [:create, :destroy]
+  end
+
+end
