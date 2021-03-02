d09aa28b128d6bbf7001debf6b0e784643654e95
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-02 23:53:25 +0000

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,17 @@ class User < ApplicationRecord
 
   has_many  :books,         dependent: :destroy
   has_many  :favorites,     dependent: :destroy
-  has_many  :book_comments
+  has_many  :book_comments, dependent: :destroy
+  
+  # ① フォローしている人取得(Userのfollowerから見た関係)
+  has_many  :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # ② フォローされている人取得(Userのfolowedから見た関係)
+  has_many  :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローしている人
+  has_many  :following_user, through: :follower, source: :followed
+  # 自分をフォローしている人(自分がフォローされている人)
+  has_many  :follower_user, through: :followed, source: :follower
+  
   attachment :profile_image
 
   validates :name, presence: true, length: {minimum: 2, maximum: 20},uniqueness: true
