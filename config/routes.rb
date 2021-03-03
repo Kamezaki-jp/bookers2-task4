e479e8cfaacc9f8ebd455664fd2fe1df712353b5
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-03 02:24:32 +0000

    add asociation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get 'home/about'
+  post 'follow/:id'   => 'relationships#follow',   as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
 
   resources :users
   resources :books do
